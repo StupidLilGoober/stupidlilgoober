eb6b3cb877462a7ee2f62d998012ac447b3daf67
Dominic Kolp <stupidlilgoober@gmail.com>
2025-09-16 07:04:58 -0400

    Delete Website directory
    I need to find an easier way to build a website. I might take a tutorial

--- Website/global js/global.js
+++
@@ -1,8 +0,0 @@
-function checkCookie(cookieName) {
-    const cookies = document.cookie;
-    if (cookies.indexOf(cookieName + "=") !== -1) {
-        return true; // Cookie exists
-    } else {
-        return false; // Cookie does not exist
-    }
-}
